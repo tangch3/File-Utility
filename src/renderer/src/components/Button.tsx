29da2e5d386b89a384e8534c1bb43a24cae661a7
Chris Tang <tangch3@hotmail.com>
2024-02-02 02:31:59 +0000

    added a button

---
+++ src/renderer/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const Button = () => {
+  return (
+    <div className="container">
+      {' '}
+      {/* Wrap buttons in a Bootstrap container */}
+      <button type="button" className="btn btn-primary">
+        Import media
+      </button>
+      {/* Other buttons */}
+    </div>
+  )
+}
